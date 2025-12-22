4b7bbd986ac0c47fed12b0ac2a28542a1868d708
Eliabe Mota <eliaberibeiro06@gmail.com>
2025-12-22 20:41:31 -0300

    sql - tested update notas

--- public/assets/sql/notas/table_notas.sql
+++ public/assets/sql/notas/table_notas.sql
@@ -25,9 +25,18 @@ VALUES(
 -- delete - tested
 DELETE FROM NOTAS WHERE ID_NOTA = 3;
 
--- update
-UPDATE `notas` SET `id`='[value-1]',`NOME_ALUNO`='[value-2]',`FAIXA_ETARIA_ALUNO`='[value-3]',`VALOR_NOTA`='[value-4]',`DATA_NOTA`='[value-5]',`created_at`='[value-6]',`updated_at`='[value-7]' WHERE 1
+-- update - tested
+UPDATE `notas` 
+	SET	`NOME_ALUNO`='Camila de Oliveira',
+	`FAIXA_ETARIA_ALUNO`='ADAD mirim',
+	`VALOR_NOTA`='5',
+	`DATA_NOTA`='2025-01-26',
+	`created_at`='2025-12-22',
+	`updated_at`='2025-12-22' 
+	WHERE `id`='5';
+
 -- select - tested
 SELECT * FROM NOTAS ORDER BY DATA_NOTA DESC;
+
 -- tested
 SELECT DATA_NOTA, VALOR_NOTA, FAIXA_ETARIA_ALUNO, NOME_ALUNO FROM NOTAS ORDER BY DATA_NOTA DESC;
