471a4304e99c3246b9dc1407387da43209aec368
Eliabe Mota <eliaberibeiro06@gmail.com>
2025-12-29 18:58:16 -0300

    php - models for system

--- app/Http/Controllers/AlunosController.php
+++ app/Http/Controllers/AlunosController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\ALuno;
 class AlunosController extends Controller
 {
     public function inicio(){
@@ -20,9 +20,19 @@ class AlunosController extends Controller
 
     public function create(Request $request){
         try{
-            return redirect("/")->with('msg', 'Aluno cadastrado com sucesso');
+            $aluno = new Aluno;
+            $aluno->NOME_ALUNO = $request->NOME_ALUNO;
+            $aluno->FAIXA_ETARIA_ALUNO = $request->FAIXA;
+            $aluno->ENDERECO_ALUNO = $request->ENDERECO_ALUNO;
+            $aluno->DATA_NASCIMENTO_ALUNO = $request->DATA_NASCIMENTO_ALUNO;
+            $aluno->CELULAR_ALUNO = $request->CELULAR_ALUNO;
+            $aluno->EMAIL_ALUNO = $request->EMAIL_ALUNO;
+
+            $aluno->save();
+
+            return redirect("/home_alunos")->with('msg', 'Aluno cadastrado com sucesso');
         }catch(Exception $e){
-            return redirect("/")->with('msg', 'Erro ao cadastrar o aluno. Tente novamente mais tarde');
+            return redirect("/home_alunos")->with('msg', 'Erro ao cadastrar o aluno. Tente novamente mais tarde');
         }
 
         
